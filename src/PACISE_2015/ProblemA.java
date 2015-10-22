4b693a5322df162865e723cb58812dc68b921d6b
Brian Fekete <bFekete@users.noreply.github.com>
2015-10-22 01:01:22 -0400

    Add Dan's code from a few weeks ago.

--- src/PACISE_2015/ProblemA.java
+++ src/PACISE_2015/ProblemA.java
@@ -3,25 +3,89 @@ package PACISE_2015;
 /**
  * PACISE 2015 Problem A - Best Fishing Pattern
  *
- * @author Brian Fekete
+ * @author Daniel Pany
  */
-public class ProblemA {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Scanner;
 
-    public static void main(String[] args) {
-
-    } // End of main
-
+public class ProblemA 
+{
+    public void Start()
+    {
+        ArrayList<String> list = new ArrayList<String>();
+        HashMap<Integer,Integer> map = new HashMap<Integer, Integer>();
+        
+        Scanner scan = new Scanner(System.in);
+        while (scan.hasNextLine())
+        {
+            String line = scan.nextLine();
+            if (line.equals("x")) break;
+            
+            
+            boolean hasFish = false;
+            for (String fish : list) 
+            {
+                if (sortOnColors(fish).equals(sortOnColors(line))) hasFish = true;
+            }
+            
+            if (!hasFish) 
+            {
+                list.add(line);
+                map.put(sortOnColors(line).hashCode(), 1);
+            }
+            else 
+            {
+                map.put(sortOnColors(line).hashCode(), map.get(sortOnColors(line).hashCode())+1);
+            }
+            
+        }
+        
+        int max = 0;
+        String bestFish = "";
+        
+        for (String fish : list) 
+        {
+            if (map.get(sortOnColors(fish).hashCode()) > max)
+            {
+                max = (int) map.get(sortOnColors(fish).hashCode());
+                bestFish = fish;
+            }
+        }
+        
+        System.out.println(bestFish);
+        
+    }
     
     
-    
-    
-    
-} // End of Class ProblemA
+    private String sortOnColors(String fishString)
+    {
+        String[] split = fishString.split("\\s+");
+        int colorCount = split.length-1;
+        String depth = split[0];
+        String[] colors;
+        colors = new String[colorCount];
+
+        for (int i = 0; i < colors.length; i++)
+        {
+            colors[i] = split[i+1];
+        }
+
+        Arrays.sort(colors);
 
 
+        String fish = depth + " ";
+        for (int i = 0; i < colors.length; i++)
+        {
+            fish += colors[i];
+            if (i != colors.length-1) fish+=" ";
+        }
 
-/* Input
- deep red white
+        return fish;
+    }
+    
+}
  surface blue white yellow
  shallow blue silver red
  medlum blue white
